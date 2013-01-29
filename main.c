52d613d4b930549cad866dd325a06279d1512194
Nevik Rehnel <hai.kataker@gmx.de>
2013-01-29 16:44:22 +0100

    Add reporting function to show progress every so many iterations

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include "sha1.h"
 
+#define REPORT_EVERY 1000000
+
 uint8_t input[20]  = {0};
 uint8_t output[20] = {0};
 
@@ -27,6 +29,16 @@ void print_input() {
     printf("\n");
 }
 
+void print_report() {
+    printf("Trying SHA ");
+    for (uint8_t i = 0; i < 20; i++) {
+        printf("%02x", input[19-i]);
+    }
+    printf("; covered %7.4f%% of search space", 0.0 /*Todo: write function to calc coverage percentage*/);
+    //Todo: calc and display ETA
+    printf("\n");
+}
+
 void handle_signal(int sig) {
     print_input();
     if (sig == SIGTERM)
@@ -37,8 +49,13 @@ int main(int argc, char const *argv[]) {
     signal(SIGTERM, handle_signal);
     signal(SIGUSR1, handle_signal);
 
+    uint32_t i = 0;
     do {
         increment_input();
+        if (i++ >= REPORT_EVERY) {
+            i = 0;
+            print_report();
+        }
         sha1_buffer((char*)input, 0, output);
     } while (memcmp(output, input, 20) != 0);
 
